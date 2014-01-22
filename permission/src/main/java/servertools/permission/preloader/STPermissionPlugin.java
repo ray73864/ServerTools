c2f6ca5ee05f8bf6a110a0f5f2849c87f79e193b
Matthew Prenger <matthewprenger@gmail.com>
2014-01-22 16:40:50 -0600

    Apache Licence v2

--- permission/src/main/java/servertools/permission/preloader/STPermissionPlugin.java
+++ permission/src/main/java/servertools/permission/preloader/STPermissionPlugin.java
@@ -5,6 +5,22 @@ import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 import java.util.Map;
 
+/*
+ * Copyright 2014 matthewprenger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @IFMLLoadingPlugin.MCVersion("1.6.4")
 public class STPermissionPlugin implements IFMLLoadingPlugin, IFMLCallHook {
 
--- src/main/java/servertools/core/command/CommandVoice.java
+++ src/main/java/servertools/core/command/CommandVoice.java
@@ -10,6 +10,22 @@ import servertools.core.lib.Strings;
 
 import java.util.List;
 
+/*
+ * Copyright 2014 matthewprenger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 public class CommandVoice extends ServerToolsCommand {
 
     public CommandVoice(String defaultName) {
