6b1e35eed516e317b1872a5c83c9fbe8b272a30b
Matthew Prenger <matthewprenger@gmail.com>
2014-01-23 15:24:59 -0600

    You should be final

--- src/main/java/servertools/core/command/ServerToolsCommand.java
+++ src/main/java/servertools/core/command/ServerToolsCommand.java
@@ -29,7 +29,7 @@ public abstract class ServerToolsCommand extends CommandBase {
         this.defaultName = defaultName;
     }
 
-    public String getCommandName() {
+    public final String getCommandName() {
 
         return name;
     }
